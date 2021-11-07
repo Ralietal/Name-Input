7898bfb518ae8069023dd351a5239a506a52460c
Sam Ralieta <samuel.ralieta@gmail.com>
2021-11-07 19:30:27 +0200

    program to take name input

--- helloWorld.java/src/helloWorld.java
+++ helloWorld.java/src/helloWorld.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Git is awesome");
+        Scanner userI = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = userI.nextLine();
+        System.out.println(name);
     }
 }
